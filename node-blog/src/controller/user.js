c7a1cf182640a8e04bb75455d742311af289606d
yangtao <istaotao@aliyun.com>
2019-11-13 14:44:35 +0800

    fix: genPassword

--- node-blog/src/controller/user.js
+++ node-blog/src/controller/user.js
@@ -2,8 +2,10 @@ const {
   exec,
   escape
 } = require('../db/mysql')
+const { genPassword } = require('../utils/crypto.js')
 
 const login = (username, password) => {
+  // password = genPassword(password)
   const sql = `
     select username, realname from users where username=${escape(username)} and password=${escape(password)};
   `
